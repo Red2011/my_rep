55b93896cfc545ee93052d58573bea128e3954ef
Red2011 <ivan.shlendin@mail.ru>
2022-03-17 12:03:56 +1000

    Added homework 2.4

---
+++ HW2/hw4.py
@@ -0,0 +1,35 @@
+"""
+Write a function that accepts another function as an argument. Then it
+should return such a function, so the every call to initial one
+should be cached.
+
+
+def func(a, b):
+    return (a ** b) ** 2
+
+
+cache_func = cache(func)
+
+some = 100, 200
+
+val_1 = cache_func(*some)
+val_2 = cache_func(*some)
+
+assert val_1 is val_2
+
+"""
+from collections.abc import Callable
+from functools import lru_cache
+
+
+def cache(func: Callable) -> Callable:
+    return lru_cache()(lambda *args: func(*args))
+
+
+def sum_of_2_component(x, y):
+    return x + y
+
+
+a = cache(sum_of_2_component)
+print(a(56, 48))
+print(a.cache_info())
